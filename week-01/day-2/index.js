d2b732b177802a75e7abd3e65d0a8692bdf22b39
Dmitry <dimonkapopov@gmail.com>
2025-09-02 19:37:53 +0300

    расширил репозиторий на 2 недели, в каждом дне песочница

---
+++ week-01/day-2/index.js
@@ -0,0 +1,20 @@
+console.log("Проверка:", document.querySelector(".js-select-operation"));
+const inputANode = document.querySelector(".js-input-a");
+const inputBNode = document.querySelector(".js-input-b");
+const selectOperationNode = document.querySelector(".js-select-operation");
+const btnResultNode = document.querySelector(".js-btn-result");
+const jsOutputNode = document.querySelector(".js-output");
+
+btnResultNode.addEventListener("click", function () {
+  const a = Number(inputANode.value);
+  const b = Number(inputBNode.value);
+  const operation = selectOperationNode.value;
+
+  const result = calculate({
+    a,
+    b,
+    operation,
+  });
+
+  console.log(result);
+});
